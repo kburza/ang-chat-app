7005757056a1ef2024c7cff79abc3f55b369116a
Kacper Burza <65431499+kburza@users.noreply.github.com>
2024-02-02 18:55:25 +0100

    Landing page fixed + build
    - landing page emulates home page
    - home page ghost icon route to login fixed

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
+  user$ = this.usersService.currentUserProfile$;
 
-  constructor() { }
+  constructor(private usersService: UsersService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
